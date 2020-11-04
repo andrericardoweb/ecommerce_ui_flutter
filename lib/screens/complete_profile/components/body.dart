27831e700f601990a73a3fe8888c5fc30fbfe8f5
Andre Ricardo <andrericardoweb@gmail.com>
2020-11-03 23:50:20 -0300

    finish - complete profile

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -1,8 +1,8 @@
-import 'package:ecommerce_ui_flutter/components/custom_surffix_icon.dart';
 import 'package:ecommerce_ui_flutter/size_config.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
+import 'complete_profile_form.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -11,81 +11,28 @@ class Body extends StatelessWidget {
       width: double.infinity,
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-        child: Column(
-          children: [
-            Text(
-              "Complete Profile",
-              style: headingStyle,
-            ),
-            Text(
-              "Complete your details or continue\nwith social media",
-              textAlign: TextAlign.center,
-            ),
-            CompleteProfileForm(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CompleteProfileForm extends StatefulWidget {
-  @override
-  _CompleteProfileFormState createState() => _CompleteProfileFormState();
-}
-
-class _CompleteProfileFormState extends State<CompleteProfileForm> {
-  final _formKey = GlobalKey<FormState>();
-  final List<String> errors = [];
-  String firstName;
-  String lastName;
-  String phoneNumber;
-  String address;
-
-  void addError({String error}) {
-    if (!errors.contains(error))
-      setState(() {
-        errors.add(error);
-      });
-  }
-
-  void removeError({String error}) {
-    if (errors.contains(error))
-      setState(() {
-        errors.remove(error);
-      });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (newValue) => firstName = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty) {
-                removeError(error: kNameNullError);
-              }
-              return null;
-            },
-            validator: (value) {
-              if (value.isEmpty) {
-                addError(error: kNameNullError);
-                return "";
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              labelText: "First Name",
-              hintText: "Enter your first name",
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CusttomSurffixIcon(svgIcon: "assets/icons/User.svg"),
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.02),
+              Text(
+                "Complete Profile",
+                style: headingStyle,
+              ),
+              Text(
+                "Complete your details or continue\nwith social media",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.05),
+              CompleteProfileForm(),
+              SizedBox(height: getProportionateScreenHeight(30)),
+              Text(
+                "By continuing your confirm that you agree\nwith our Term and Condition",
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/screens/complete_profile/components/complete_profile_form.dart
@@ -0,0 +1,151 @@
+import 'package:ecommerce_ui_flutter/components/custom_surffix_icon.dart';
+import 'package:ecommerce_ui_flutter/components/default_button.dart';
+import 'package:ecommerce_ui_flutter/components/form_error.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class CompleteProfileForm extends StatefulWidget {
+  @override
+  _CompleteProfileFormState createState() => _CompleteProfileFormState();
+}
+
+class _CompleteProfileFormState extends State<CompleteProfileForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+  String firstName;
+  String lastName;
+  String phoneNumber;
+  String address;
+
+  void addError({String error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildFirstNameFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildLastNameFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPhoneNumberFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildAddressFormField(),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(40)),
+          DefaultButton(
+            text: "Continue",
+            press: (){
+              if (_formKey.currentState.validate()) {
+                //Go to screen
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildAddressFormField() {
+    return TextFormField(
+          onSaved: (newValue) => address = newValue,
+          onChanged: (value) {
+            if (value.isNotEmpty) {
+              removeError(error: kAddressNullError);
+            }
+            return null;
+          },
+          validator: (value) {
+            if (value.isEmpty) {
+              addError(error: kAddressNullError);
+              return "";
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            labelText: "Address",
+            hintText: "Enter your Address",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CusttomSurffixIcon(svgIcon: "assets/icons/Location point.svg"),
+          ),
+        );
+  }
+
+  TextFormField buildPhoneNumberFormField() {
+    return TextFormField(
+          keyboardType: TextInputType.number,
+          onSaved: (newValue) => phoneNumber = newValue,
+          onChanged: (value) {
+            if (value.isNotEmpty) {
+              removeError(error: kPhoneNumberNullError);
+            }
+            return null;
+          },
+          validator: (value) {
+            if (value.isEmpty) {
+              addError(error: kPhoneNumberNullError);
+              return "";
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            labelText: "Phone Number",
+            hintText: "Enter your phone number",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CusttomSurffixIcon(svgIcon: "assets/icons/Phone.svg"),
+          ),
+        );
+  }
+
+  TextFormField buildLastNameFormField() {
+    return TextFormField(
+          onSaved: (newValue) => lastName = newValue,
+          decoration: InputDecoration(
+            labelText: "Last Name",
+            hintText: "Enter your last name",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CusttomSurffixIcon(svgIcon: "assets/icons/User.svg"),
+          ),
+        );
+  }
+
+  TextFormField buildFirstNameFormField() {
+    return TextFormField(
+          onSaved: (newValue) => firstName = newValue,
+          onChanged: (value) {
+            if (value.isNotEmpty) {
+              removeError(error: kNameNullError);
+            }
+            return null;
+          },
+          validator: (value) {
+            if (value.isEmpty) {
+              addError(error: kNameNullError);
+              return "";
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            labelText: "First Name",
+            hintText: "Enter your first name",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CusttomSurffixIcon(svgIcon: "assets/icons/User.svg"),
+          ),
+        );
+  }
+}
